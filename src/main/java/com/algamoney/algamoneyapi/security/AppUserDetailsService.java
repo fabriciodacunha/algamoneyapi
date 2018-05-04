ba79f3d8cade203855e989b72ee07b60da293577
Fabrício da Cunha Oliveira <fabricio.cunha@mpro.mp.br>
2018-05-04 13:30:48 -0400

    adicionando profiles

--- src/main/java/com/algamoney/algamoneyapi/security/AppUserDetailsService.java
+++ src/main/java/com/algamoney/algamoneyapi/security/AppUserDetailsService.java
@@ -8,7 +8,6 @@ import java.util.Set;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -19,15 +18,15 @@ import com.algamoney.algamoneyapi.repository.UsuarioRepository;
 
 @Service
 public class AppUserDetailsService implements UserDetailsService {
-	
-	@Autowired
-	public UsuarioRepository usuarioRepository;
 
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+	
 	@Override
 	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
 		Optional<Usuario> usuarioOptional = usuarioRepository.findByEmail(email);
-		Usuario usuario = usuarioOptional.orElseThrow(()-> new UsernameNotFoundException("Usuário e/ou senha incorretos"));
-		return new User(email, usuario.getSenha(), getPermissoes(usuario));
+		Usuario usuario = usuarioOptional.orElseThrow(() -> new UsernameNotFoundException("Usuário e/ou senha incorretos"));
+		return new UsuarioSistema(usuario, getPermissoes(usuario));
 	}
 
 	private Collection<? extends GrantedAuthority> getPermissoes(Usuario usuario) {
