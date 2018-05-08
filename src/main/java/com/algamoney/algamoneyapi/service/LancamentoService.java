f57ef3ac6acec66953657cfcc9f86ee4ad604bc4
Fabrício da Cunha Oliveira <fabricio.cunha@mpro.mp.br>
2018-05-08 13:59:42 -0400

    inserindo lancamentos

--- src/main/java/com/algamoney/algamoneyapi/service/LancamentoService.java
+++ src/main/java/com/algamoney/algamoneyapi/service/LancamentoService.java
@@ -1,5 +1,6 @@
 package com.algamoney.algamoneyapi.service;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,21 +12,47 @@ import com.algamoney.algamoneyapi.service.exception.PessoaInexistenteOuInativaEx
 
 @Service
 public class LancamentoService {
-	
+
 	@Autowired
-	private PessoaRepository pessoaRepository ;
-	
+	private PessoaRepository pessoaRepository;
+
 	@Autowired
-	private LancamentoRepository lancamentoRepository ;
+	private LancamentoRepository lancamentoRepository;
 
 	public Lancamento salvar(Lancamento lancamento) {
-		Pessoa pessoa = pessoaRepository.findOne(lancamento.getPessoa().getCodigo());
-		
-		if(pessoa == null || pessoa.isInativo()) {
+		validarPessoa(lancamento);
+
+		return lancamentoRepository.save(lancamento);
+	}
+
+	public Lancamento atualizar(Long codigo, Lancamento lancamento) {
+		Lancamento lancamentoSalvo = buscarLancamentoExistente(codigo);
+		if (!lancamento.getPessoa().equals(lancamentoSalvo.getPessoa())) {
+			validarPessoa(lancamento);
+		}
+
+		BeanUtils.copyProperties(lancamento, lancamentoSalvo, "codigo");
+
+		return lancamentoRepository.save(lancamentoSalvo);
+	}
+
+	private void validarPessoa(Lancamento lancamento) {
+		Pessoa pessoa = null;
+		if (lancamento.getPessoa().getCodigo() != null) {
+			pessoa = pessoaRepository.findOne(lancamento.getPessoa().getCodigo());
+		}
+
+		if (pessoa == null || pessoa.isInativo()) {
 			throw new PessoaInexistenteOuInativaException();
 		}
-			
-		return lancamentoRepository.save(lancamento);
+	}
+
+	private Lancamento buscarLancamentoExistente(Long codigo) {
+		Lancamento lancamentoSalvo = lancamentoRepository.findOne(codigo);
+		if (lancamentoSalvo == null) {
+			throw new IllegalArgumentException();
+		}
+		return lancamentoSalvo;
 	}
 
 }
